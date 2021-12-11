8d1fb50f1fb048ebb55a0f8559ba36178d8d97ad
argraur <argraur@gmail.com>
2021-12-11 14:29:56 +0800

    laurel_sprout: props: Enable Rich-Communication Services
    * Get rid of spam log of unable to start 'com.qualcomm.qti.uceservice@2.3::IUceService/com.qualcomm.qti.uceservice'

--- props.mk
+++ props.mk
@@ -284,3 +284,68 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.bt.enableAptXHD=true \
+    persist.vendor.bt.a2dp.aac_whitelist=false \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
+    persist.vendor.btstack.enable.splita2dp=true \
+    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.enable.twsplussho=true \
+    persist.service.btui.use_aptx=1 \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
+    vendor.bluetooth.soc=cherokee
+
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.quick.charge=1 \
+    ro.charger.enable_suspend=true
+
+# CPU
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bionic.cpu_variant=cortex-a73 \
+    ro.bionic.2nd_cpu_variant=cortex-a73 \
+    dalvik.vm.isa.arm64.variant=cortex-a73 \
+    dalvik.vm.isa.arm.variant=cortex-a73
+
+# Dex2oat
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true
+
+# DRM
+PRODUCT_PRODUCT_PROPERTIES += \
+    drm.service.enabled=true
+
+# Graphics
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
+# IMS
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
+# Kill off IMS logspam with fire
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.ims.disableADBLogs=1 \
+    persist.vendor.ims.disableDebugLogs=1 \
+    persist.vendor.ims.disableIMSLogs=1 \
+    persist.vendor.ims.disableQXDMLogs=1
+
+# Media
+PRODUCT_PRODUCT_PROPERTIES += \
+    vendor.audio.offload.buffer.size.kb=256
+
+# Netflix custom property
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.netflix.bsp_rev=Q6115-31409-1
+
+# RCS
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.rcs.supported=1
+
+# System
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.binary_xml=false
