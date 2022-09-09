2fd46796b0cc2c7603b133419d2753bd7f5f90db
Musical143143 <basub4615@gmail.com>
2022-09-09 08:22:07 +0000

    laurel_sprout: wellcome CherishOs13 Official

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_laurel_sprout.mk
+    $(LOCAL_DIR)/cherish_laurel_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_laurel_sprout-user \
-    evolution_laurel_sprout-userdebug \
-    evolution_laurel_sprout-eng
+    cherish_laurel_sprout-user \
+    cherish_laurel_sprout-userdebug \
+    cherish_laurel_sprout-eng
--- cherish_laurel_sprout.mk
+++ cherish_laurel_sprout.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Evolution-X stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common CherishOs stuff
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, $(LOCAL_PATH)/laurel_sprout.mk)
@@ -28,7 +28,7 @@ $(call inherit-product, $(LOCAL_PATH)/laurel_sprout.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := evolution_laurel_sprout
+PRODUCT_NAME := cherish_laurel_sprout
 PRODUCT_MODEL := Mi A3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -37,20 +37,24 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 SKIP_ABI_CHECKS := true
 
 # FOD Animation
+EXTRA_UDFPS_ANIMATIONS := true
 EXTRA_FOD_ANIMATIONS := true
+HAS_FOD := true
 
 # Props
-EVO_MAINTAINER := HemantSachdeva
-EVO_BUILD_TYPE := OFFICIAL
-EVO_SUPPORT_URL := https://t.me/EvolutionXLaurel_Sprout
-EVO_DONATE_URL := https://pages.razorpay.com/HemantSachdevaa
-BUILD_USERNAME := Hemant
-BUILD_HOSTNAME := Evolution-X
-
-WITH_GAPPS := true
+WITH_GMS := true
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_MINI_GAPPS := true
-EVO_SIGNED := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Pixel charging animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Cherish Official
+CHERISH_BUILD_TYPE := OFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=BASUBHAJANTRI
+
