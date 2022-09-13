9da8785d51a995e6bc6e3623c1a818f1f01c7413
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-09-13 20:42:37 +0530

    laurel_sprout: props: disable sf cache for smoothness

--- props.mk
+++ props.mk
@@ -159,6 +159,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_client_composition_cache=1 \
     debug.sdm.support_writeback=0 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
