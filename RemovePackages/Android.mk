4cc710a710c17045d478e3e8b93b0f13c0cbf686
Musical143143 <basub4615@gmail.com>
2022-09-16 21:14:10 +0000

    Revert "laurel_sprout: RemovePackages: Drop more Gapps"
    This reverts commit 97a70405f7f7f88451b8dd2fe654f76171b579bd.

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,11 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices Music
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
 LOCAL_OVERRIDES_PACKAGES := \
 	CalendarGooglePrebuilt \
 	Camera2 \
@@ -23,107 +18,6 @@ LOCAL_OVERRIDES_PACKAGES := \
 	USCCDM \
 	VZWAPNLib \
 	VzwOmaTriger \
-        PixelBuds \
-        PixelLiveWallpaperPrebuilt \
-        RecorderPrebuilt \
-        SafetyHubPrebuilt \
-        SCONE \
-        ScribePrebuilt \
-        Showcase \
-        SoundAmplifierPrebuilt \
-        SprintDM \
-        SprintHM \
-        Tycho \
-        USCCDM \
-        Via \
-        Videos \
-        VZWAPNLib \
-        VzwOmaTrigger \
-        YouTube \
-        YouTubeMusicPrebuilt \
-        AmbientSensePrebuilt \
-        arcore \
-        ConnMO \
-        ConnMetrics \
-        DCMO \
-        DevicePolicyPrebuilt \
-        DMService \
-        GCS \
-        GoogleTTS \
-        Maps \
-        MyVerizonServices \
-        OBDM_Permissions \
-        obdm_stub \
-        OdadPrebuilt \
-        OemDmTrigger \
-        OPScreenRecord \
-        Ornament \
-        PixelBuds \
-        PixelLiveWallpaperPrebuilt \
-        RecorderPrebuilt \
-        SafetyHubPrebuilt \
-        SCONE \
-        ScribePrebuilt \
-        Showcase \
-        SoundAmplifierPrebuilt \
-        SprintDM \
-        SprintHM \
-        Tycho \
-        USCCDM \
-        Via \
-        Videos \
-        VZWAPNLib \
-        VzwOmaTrigger \
-        YouTube \
-        YouTubeMusicPrebuilt \
-        AmbientSensePrebuilt \
-	AppDirectedSMSService \
-	CalendarGooglePrebuilt \
-	Camera2 \
-	CarrierSetup \
-	ConnMO \
-	DCMO \
-	DMService \
-	DevicePolicyPrebuilt \
-	DiagnosticsToolPrebuilt \
-	Drive \
-	HelpRtcPrebuilt \
-	Maps \
-	MyVerizonServices \
-	NgaResources \
-	OBDM_Permissions \
-	OemDmTrigger \
-	PixelLiveWallpaperPrebuilt \
-	PixelWallpapers2021 \
-	PrebuiltGmail \
-	RecorderPrebuilt \
-	SafetyHubPrebuilt \
-	ScribePrebuilt \
-	Showcase \
-	SoundAmplifierPrebuilt \
-	SprintDM \
-	SprintHM \
-	Tycho \
-	USCCDM \
-	VZWAPNLib \
-	VzwOmaTriger \
-        AmbientSensePrebuilt \
-        arcore \
-        ConnMO \
-        ConnMetrics \
-        DCMO \
-        DevicePolicyPrebuilt \
-        DMService \
-        GCS \
-        GoogleTTS \
-        Maps \
-        MyVerizonServices \
-        OBDM_Permissions \
-        obdm_stub \
-        OdadPrebuilt \
-        OemDmTrigger \
-        OPScreenRecord \
-        Ornament \
         PixelBuds \
         PixelLiveWallpaperPrebuilt \
         RecorderPrebuilt \
